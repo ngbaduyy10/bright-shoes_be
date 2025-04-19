5090ffa44e8a32aa01ee5ef32755df57a6eb7a7b
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-04-19 10:09:21 +0700

    updated

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -94,13 +94,12 @@ module.exports.adminLogin = async (req, res) => {
 }
 
 module.exports.adminLogout = async (req, res) => {
-    res.cookie("token", "", {
+    return res.clearCookie("token", {
         httpOnly: true,
         secure: true,
         sameSite: "none",
         path: "/",
         domain: process.env.DOMAIN,
-        expires: new Date(0),
     }).status(200).json({
         success: true,
         message: "Logout successful"
