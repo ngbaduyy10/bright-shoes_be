2b101807ce580e6961d08901aacb9e26bc069289
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-04-23 17:23:35 +0700

    upload cloud fixed

--- api/middlewares/uploadFile.middleware.js
+++ api/middlewares/uploadFile.middleware.js
@@ -1,7 +1,6 @@
 const uploadToCloud = require('../../helpers/uploadToCloud');
 
 module.exports.uploadFile = async (req, res, next) => {
-    console.log(req);
     if (req.file) {
         const result = await uploadToCloud.upload(req.file.buffer);
         req.body[req.file.fieldname] = result;
