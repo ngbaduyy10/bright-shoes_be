fcda1752c74df53479c5c137218a75576b70f31a
ngbaduyy10 <ngbaduyy05@gmail.com>
2025-04-24 15:31:18 +0700

    discount api updated

--- api/controllers/discount.controller.js
+++ api/controllers/discount.controller.js
@@ -15,3 +15,21 @@ module.exports.getDiscounts = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 }
+
+module.exports.getDiscountByOrderValue = async (req, res) => {
+    try {
+        const { orderValue } = req.body;
+        const sql = `
+            SELECT * FROM discount
+            WHERE min_order_value <= ?
+        `;
+
+        const [discounts] = await db.query(sql, [orderValue]);
+        return res.status(200).json({
+            success: true,
+            data: discounts,
+        });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
--- api/routes/discount.route.js
+++ api/routes/discount.route.js
@@ -4,4 +4,6 @@ const discountController = require('../controllers/discount.controller');
 
 router.get('/', discountController.getDiscounts);
 
+router.post('/value', discountController.getDiscountByOrderValue);
+
 module.exports = router;
